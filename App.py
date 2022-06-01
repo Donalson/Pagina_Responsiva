ceb5b5c87e74e550e3e233e0e6e9ef2f4df9d7b0
Donalson Javier <donalson1517@gmail.com>
2022-06-01 10:10:12 -0600

    Creacion de rutas y estructuras html basicas

---
+++ App.py
@@ -0,0 +1,26 @@
+#Importacion de flask para crear y ejecutar sitio web, renderizar plantillas
+from flask import Flask, render_template
+
+#Instanciacion de flask dentro de la variable app
+app = Flask(__name__)
+
+#Creacion de la ruta principal del sitio web
+@app.route('/')
+def Inicio():
+    return render_template('Index.html')
+
+@app.route("/Productos")
+def Productos():
+    return render_template('Productos.html')
+
+@app.route("/ServicioTecnico")
+def Servicio():
+    return render_template('Servicio.html')
+
+@app.route("/Contactanos")
+def Contactanos():
+    return render_template('Contactanos.html')
+
+#Inializacion del sitio web para correr(modo debug encendido para detectar cambios)
+if __name__ == '__main__':
+    app.run(debug=True)
